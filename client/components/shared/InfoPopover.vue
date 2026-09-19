a1a4abd489d55dc393d9cb9b878f0bc53fa7ed58
Florian Moser <git@famoser.ch>
2026-09-19 07:06:01 +0200

    fix: Ensure popover fully shown

--- client/components/shared/InfoPopover.vue
+++ client/components/shared/InfoPopover.vue
@@ -6,6 +6,7 @@ defineProps<{
   popover: string
 }>()
 
+const iconClicked = ref(false)
 const iconHovered = ref(false)
 const popoverHovered = ref(false)
 </script>
@@ -17,39 +18,18 @@ const popoverHovered = ref(false)
       <FontAwesomeIcon
         :icon="['far', 'circle-question']"
         role="button"
+        @click="iconClicked = !iconClicked"
         @mouseover="iconHovered = true"
         @focusin="iconHovered = true"
         @mouseleave="iconHovered = false"
         @focusout="iconHovered = false"
       />
-      <span class="position-absolute position-correction" v-if="popoverHovered || iconHovered">
-        <span
-          class="popover d-block bs-popover-auto fade show position-absolute top-100 start-50 translate-middle-x mt-2"
-          data-popper-placement="bottom"
-          @mouseover="popoverHovered = true"
-          @mouseleave="popoverHovered = false"
-        >
-          <span class="popover-arrow position-absolute top-0 start-50 translate-middle"></span>
-        </span>
-        <span class="popover d-block bs-popover-auto fade show position-absolute top-100 start-50 translate-middle-x mt-2 mw-none" data-popper-placement="bottom" role="tooltip">
-          <span class="popover-body d-block w-35em">
-            {{ popover }}
-          </span>
-        </span>
+      <span class="d-block alert alert-info" v-if="popoverHovered || iconHovered || iconClicked" @mouseover="popoverHovered = true" @mouseleave="popoverHovered = false">
+        <FontAwesomeIcon :icon="['far', 'circle-question']" />
+        {{ popover }}
       </span>
     </span>
   </div>
 </template>
 
-<style scoped>
-.w-35em {
-  width: 35em;
-}
-.mw-none {
-  max-width: none;
-}
-.position-correction {
-  top: 1em;
-  left: 0.5em;
-}
-</style>
+<style scoped></style>
